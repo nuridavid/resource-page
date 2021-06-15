50f3aa6977e1f44a71aaa550140b15db865e1d7e
nuridavid <nurieldavid@gmail.com>
2021-06-15 15:07:04 -0400

    basic file structure and components

---
+++ public/Components/Navy/Navy.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Navy.scss";
+function Navy() {
+  return <div></div>;
+}
+
+export default Navy;
---
+++ public/Components/Websites/Websites.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Websites.scss";
+function Websites() {
+  return <div></div>;
+}
+
+export default Websites;
